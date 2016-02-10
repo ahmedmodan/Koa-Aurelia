3ab2eeb397b6c2e074019f00a74591dc25842d11
Ahmed Modan <ahmed.modan@gmail.com>
2016-02-10 02:06:18 -0800

    [chore] log simple generator functions

--- app.js
+++ app.js
@@ -6,5 +6,47 @@ app.use(function* () {
   this.body = 'hello world';
 });
 
+
+function* print10() {
+  yield 1;
+  yield 2;
+  yield 3;
+  yield 4;
+  yield 5;
+  yield 6;
+  yield 7;
+  yield 8;
+  yield 9;
+  yield 10;
+}
+
+for (let num of print10()) {
+  console.log(num);
+}
+
+const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+
+function* printAlphabet(str) {
+  let index = 0;
+  while (true) {
+    let current = str[index];
+    yield current;
+    index++;
+    if (index === 26) {
+      index = 0;
+      yield '';
+      yield `the alphabet: ${str}! \n`;
+    }
+  }
+}
+
+let printedAlphabet = printAlphabet(alphabet);
+
+function logAlphabet() {
+  console.log(printedAlphabet.next().value);
+}
+
+setInterval(logAlphabet, 300);
+
+
 app.listen(3000);
-console.log('koa server listening on port 3000');
