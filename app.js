c2a0c808bedf3df2239293c8a2306efcad7fea33
Ahmed Modan <ahmed.modan@gmail.com>
2016-02-10 21:55:12 -0800

    [refactor] add basic routes

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 import koa from 'Koa';
+import koaRouter from 'koa-router';
 import generators from './server/generators/generatorExports';
-
-
-const app = koa();
-
-app.use(function *() {
-  this.body = 'hello world';
-});
 
 generators.iterator();
 generators.alphabet();
 generators.errorTest();
+
+const router = koaRouter();
+const app = koa();
+
+router.get('/', function *() {
+  this.body = 'This is the home page!';
+});
+
+router.get('/secret', function *() {
+  this.body = 'This is the secret page!';
+});
+
+app.use(router.routes());
 
 app.listen(process.env.PORT || 3000);
 console.log('Koa listening on port 3000');
